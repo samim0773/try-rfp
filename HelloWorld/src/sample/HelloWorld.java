66026d76dd150caff638f3a215edd0c50bc3bac2
Samim Aktar <samimaktar0773@gmail.com>
2021-09-28 11:21:27 +0530

    Print Hello World

---
+++ HelloWorld/src/sample/HelloWorld.java
@@ -0,0 +1,38 @@
+package sample;
+
+import java.util.Scanner;
+
+public class HelloWorld {
+	public static void main(String args[]) {
+	
+		System.out.println("Hello World, Welcome to Java Online Course!");
+		System.out.println(" add from github");
+		
+//		take input from user
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter First Number: ");
+		int num1 = sc.nextInt();
+		
+		System.out.println("Enter Second Number: ");
+		int num2 = sc.nextInt();
+		
+//		method calling
+		int res = addition(num1, num2);
+		System.out.println("The addition of two number is : " + res);
+		
+	    sc.close();
+	}
+	
+	
+//	Method signature
+//	AccessModifier AccessSpecifire(optional) ReturnType MethodName
+	
+	public static int addition(int a, int b) {
+		int sum = a + b;
+//		System.out.println("The addition of two number is : " + sum);
+		return sum;
+		
+	}
+
+}
